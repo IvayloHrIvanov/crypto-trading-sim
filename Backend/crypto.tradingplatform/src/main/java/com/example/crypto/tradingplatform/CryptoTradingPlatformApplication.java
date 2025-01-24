12514cc3959c5d40eb7a550039886d9ac949c868
IvayloHrIvanov <ivaylo.ivanov3107@gmail.com>
2025-01-24 21:14:42 +0200

    build: Commit initial backend blueprint and structure

---
+++ Backend/crypto.tradingplatform/src/main/java/com/example/crypto/tradingplatform/CryptoTradingPlatformApplication.java
@@ -0,0 +1,11 @@
+package com.example.crypto.tradingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTradingPlatformApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTradingPlatformApplication.class, args);
+	}
+}
