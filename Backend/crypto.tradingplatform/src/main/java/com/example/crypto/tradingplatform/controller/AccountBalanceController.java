3b15031cdfd650f6539adcd2b68a92f960bd4e84
IvayloHrIvanov <ivaylo.ivanov3107@gmail.com>
2025-01-30 13:56:44 +0200

    refactor: Refactor request mapping and method name following conventions

--- Backend/crypto.tradingplatform/src/main/java/com/example/crypto/tradingplatform/controller/AccountBalanceController.java
+++ Backend/crypto.tradingplatform/src/main/java/com/example/crypto/tradingplatform/controller/AccountBalanceController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173/")
-@RequestMapping("/api/accountBalance")
+@RequestMapping("/api/account-balance")
 public class AccountBalanceController {
 
     private final AccountBalanceService accountBalanceService;
--- Backend/crypto.tradingplatform/src/main/java/com/example/crypto/tradingplatform/controller/CryptoController.java
+++ Backend/crypto.tradingplatform/src/main/java/com/example/crypto/tradingplatform/controller/CryptoController.java
@@ -5,18 +5,20 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173/")
+@RequestMapping("/api/crypto-prices")
 public class CryptoController {
     @Autowired
     private KrakenWebSocketService krakenWebSocketService;
 
-    @GetMapping("/api/crypto-prices")
-    public Map<String, JsonNode> getCryptoPrices() {
+    @GetMapping
+    public Map<String, JsonNode> getAllCryptoPrices() {
         return krakenWebSocketService.getCryptoPrices();
     }
 }
